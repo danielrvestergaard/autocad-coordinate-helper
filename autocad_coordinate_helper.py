f20125bd4d6493bd90a713a4fc224e1c2f27f2bc
danielrvestergaard <daniel_vestergaard94@hotmail.com>
2026-02-03 13:30:58 +0100

    Comment out unused imports
    Comment out unused imports for re and Tk.

--- autocad_coordinate_helper.py
+++ autocad_coordinate_helper.py
@@ -2,10 +2,10 @@ import pyautogui
 import pyperclip
 import numpy as np
 import pandas as pd
-import re
+#import re
 from pynput import mouse, keyboard
 from ipywidgets import widgets
-from tkinter import filedialog, Tk
+#from tkinter import filedialog, Tk
 
 class AutoCAD_Coordinate_Helper:
 
